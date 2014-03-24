f7190442f7377b6deb1ec456f245106d17619a95
Guilherme Vieira <super.driver.512@gmail.com>
2014-03-24 00:21:45 -0300

    Add copyright notice to other source files.

--- forever-octopus.js
+++ forever-octopus.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
+/*
+	Copyright (c) 2014 Guilherme Prá Vieira
+
+	This file is part of Octopus.
+
+	Octopus is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of
+	the License, or (at your option) any later version.
+
+	Octopus is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public
+	License along with Octopus. If not, see <http://www.gnu.org/licenses/>.
+*/
 var spawn = require('child_process').spawn;
 var server = spawn (
 	__dirname + '/node_modules/forever/bin/forever'
